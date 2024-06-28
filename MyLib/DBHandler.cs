06611487e31f33c04b13c781b17db97531b1f98d
Eileen9876 <104195959+Eileen9876@users.noreply.github.com>
2024-06-29 01:50:59 +0800

    Add files via upload

---
+++ MyLib/DBHandler.cs
@@ -0,0 +1,158 @@
+﻿using System.Data.OleDb;
+using System.Data;
+using System.Collections.Generic;
+using System.IO;
+
+namespace MyLib
+{
+    public class Parameter
+    {
+        public OleDbType type = OleDbType.Empty;
+        public int count = 0;
+        public List<string> col = new List<string>(); 
+        public List<object> val = new List<object>();
+    }
+
+    public class DBHandler
+    {
+        private OleDbConnection sqlConn = null;
+
+        ~DBHandler()
+        {
+            if (sqlConn != null) sqlConn.Dispose();
+        }
+
+        public void Connect(string dbfile_path)
+        {
+            if (!File.Exists(dbfile_path)) throw new System.Exception(dbfile_path + "檔案不存在");
+
+            if (sqlConn != null)
+            {
+                sqlConn.Dispose();
+                sqlConn = null;
+            }
+
+            sqlConn = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + dbfile_path);
+        }
+
+        private OleDbCommand Create_SelectCommand(string table_name, string[] col, string condition = "")
+        {   
+            // SELECT column1, column2, ...
+            // FROM table_name
+            // WHERE condition;
+
+            string query = "SELECT " + col[0];
+            for (int i = 1; i < col.Length; i++) query += ", " + col[i];
+
+            query += " FROM " + table_name;
+
+            if (condition == "") query += ";";
+            else query += " WHERE " + condition + ";";
+
+            return new OleDbCommand(query, this.sqlConn);
+        }
+
+        private OleDbCommand Create_InsertCommand(string table_name, string[] col, string[] val, Parameter param = null)
+        {
+            // INSERT INTO table_name (column1, column2, column3, ... , param_column1, param_column2)
+            // VALUES (value1, value2, value3, ... , @param_column1, @param_column2);
+
+            string query = "INSERT INTO " + table_name;
+
+            query += " (" + col[0];
+            for (int i = 1; i < col.Length; i++) query += ", " + col[i];
+            if (param != null) foreach (string param_col in param.col) query += ", " + param_col;
+            query += ")";
+
+            query += " VALUES ('" + val[0] + "'";
+            for (int i = 1; i < val.Length; i++) query += ", '" + val[i] + "'";
+            if (param != null) foreach(string param_col in param.col) query += ", @" + param_col;
+            query += ");";
+
+            return new OleDbCommand(query, this.sqlConn);
+        }
+
+        private OleDbCommand Create_DeleteCommand(string table_name, string condition)
+        {
+            string query = "DELETE FROM " + table_name + " WHERE " + condition + ";";
+            return new OleDbCommand(query, this.sqlConn);
+        }
+
+        private OleDbCommand Create_UpdateCommand(string table_name, string[] col, string[] val, string condition, Parameter param = null)
+        {
+            // UPDATE table_name
+            // SET column1 = value1, column2 = value2, ..., param_column1 = @param_column1, param_column2 = @param_column1
+            // WHERE condition;
+
+            string query = "UPDATE " + table_name + " SET " + col[0] + " = '" + val[0] + "'";
+            
+            for(int i = 1; i < col.Length;i++) query += ", " + col[i] + " = '" + val[i] + "'";
+
+            if (param != null) foreach (string param_col in param.col) query += ", " + param_col + " = @" + param_col;
+
+            query += " WHERE " + condition;
+
+            return new OleDbCommand(query, this.sqlConn);
+        }
+
+        private void Execute(OleDbCommand cmd)
+        {
+            try 
+            {
+                this.sqlConn.Open();
+                cmd.ExecuteNonQuery(); 
+            }
+            catch { throw; }
+            finally { this.sqlConn.Close(); }
+        }
+
+        private void Proc_Parameter(OleDbCommand cmd, Parameter param)
+        {
+            for (int i = 0; i < param.count; i++)
+                cmd.Parameters.Add(param.col[i], param.type).Value = param.val[i];
+        }
+
+        public DataTable Select(string table_name, string[] col, string condition = "")
+        {
+            OleDbCommand cmd = Create_SelectCommand(table_name, col, condition);
+            Execute(cmd);
+
+            OleDbDataAdapter dp = new OleDbDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            dp.Fill(dt);
+
+            return dt;
+        }
+
+        /// <param name="col">表單欄位，不包含參數對應的欄位</param>
+        /// <param name="val">表單欄位對應的值</param>
+        /// <param name="param">參數對應的欄位與值</param>
+        public void Insert(string table_name, string[] col, string[] val, Parameter param = null)
+        {
+            OleDbCommand cmd = Create_InsertCommand(table_name, col, val, param);
+
+            if (param != null) Proc_Parameter(cmd, param);
+
+            Execute(cmd);
+        }
+
+        public void Delete(string table_name, string condition)
+        {
+            OleDbCommand cmd = Create_DeleteCommand(table_name, condition);
+            Execute(cmd);
+        }
+
+        /// <param name="col">表單欄位，不包含參數對應的欄位</param>
+        /// <param name="val">表單欄位對應的值</param>
+        /// <param name="param">參數對應的欄位與值</param>
+        public void Update(string table_name, string[] col, string[] val, string condition, Parameter param = null)
+        {
+            OleDbCommand cmd = Create_UpdateCommand(table_name, col, val, condition, param);
+
+            if (param != null) Proc_Parameter(cmd, param);
+
+            Execute(cmd);
+        }
+
+    }
+}
---
+++ MyLib/DataGridView_Operator.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace MyLib
+{
+    struct Merge_Info
+    {
+        public string title;
+        public Rectangle rec;
+    }
+
+    public class DataGridView_Operator
+    {
+        private readonly DataGridView view;
+        private List<Merge_Info> merge_info_list = new List<Merge_Info>(); // 主標頭資訊
+        private int offset;  // 滾動滾輪後相對於起始點的偏移量
+
+        public DataGridView_Operator(DataGridView view)
+        {
+            this.view = view;
+
+            this.view.Scroll += (sender, e) => 
+            {
+                if (e.ScrollOrientation == ScrollOrientation.VerticalScroll) return;
+
+                offset = e.NewValue;
+                this.view.Invalidate();
+            };
+
+            this.view.Paint += (sender, e) =>
+            {
+                foreach (Merge_Info info in merge_info_list) Draw(e, info.rec, info.title);
+            };
+
+            Set_HeaderProperties();
+
+            Optimize_Paint();
+        }
+
+        private void Set_HeaderProperties()
+        {
+            this.view.RowHeadersVisible = false;
+            this.view.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.EnableResizing;
+            this.view.ColumnHeadersHeight *= 3;
+            this.view.ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.BottomCenter;
+        }
+
+        private void Optimize_Paint()
+        {
+            Type type = this.view.GetType();
+            PropertyInfo info = type.GetProperty("DoubleBuffered", BindingFlags.Instance | BindingFlags.NonPublic);
+            info.SetValue(this.view, true, null);
+        }
+
+        /// <param name="header">DataGridView 的標頭</param>
+        /// <param name="table">要寫入 DataGridView 的資料</param>
+        /// <param name="table_col">參數 table 的欄位名稱，用於排序 DataGridView</param>
+        public void Set_Data(List<KeyValuePair<string, string[]>> header, DataTable table, List<string> table_col)
+        {
+            merge_info_list.Clear();
+            offset = 0;
+
+            this.view.DataSource = table;
+
+            Sort_Column(table_col);
+
+            Set_Header(header);
+
+            Cancel_SortMode();
+        }
+
+        public void Sort_Column(List<string> col)
+        {
+            // 調整 DataGridView 的資料顯示順序
+            for(int i = 0; i < col.Count; i++) this.view.Columns[col[i]].DisplayIndex = i;
+        }
+
+        private void Set_Header(List<KeyValuePair<string, string[]>> header)
+        {
+            // 設置主標頭資訊於 merge_info_list 用於後續繪製標頭
+            // 副標頭直接寫入 DataGridView
+
+            int rec_x = 0;
+            int rec_height = this.view.ColumnHeadersHeight;
+            int idx = 0;
+            foreach (KeyValuePair<string, string[]> pair in header)
+            {
+                this.view.Columns[idx].HeaderText = (pair.Value == null) ? pair.Key : pair.Value[0]; 
+
+                Merge_Info info = new Merge_Info  // 主標頭資訊設置
+                {
+                    title = pair.Key, // 主標頭文字
+                    rec = new Rectangle
+                    {
+                        X = rec_x,
+                        Y = 0,
+                        Width = this.view.Columns[idx].Width,
+                        Height = rec_height
+                    }
+                };              
+
+                idx++;
+
+                if (pair.Value != null) 
+                {
+                    info.rec.Height /= 2;
+                    for (int i = 1; i < pair.Value.Length; i++)
+                    {
+                        this.view.Columns[idx].HeaderCell.Value = pair.Value[i]; // 副標頭文字設置
+                        info.rec.Width += this.view.Columns[idx].Width; // 主標頭寬度設置
+                        idx++;
+                    }
+                }
+
+                merge_info_list.Add(info);
+                rec_x += info.rec.Width;
+            }
+        }
+
+        private void Draw(PaintEventArgs e, Rectangle rec, string title)
+        {
+            rec.X -= offset; // 滾動滾輪後的偏移位置
+            using (Brush back = new SolidBrush(this.view.ColumnHeadersDefaultCellStyle.BackColor))
+            using (Brush fore = new SolidBrush(this.view.ColumnHeadersDefaultCellStyle.ForeColor))
+            using (Pen p = new Pen(this.view.GridColor))
+            using (StringFormat format = new StringFormat())
+            {
+                format.Alignment = StringAlignment.Center;
+                format.LineAlignment = StringAlignment.Center;
+
+                e.Graphics.FillRectangle(back, rec);
+                e.Graphics.DrawRectangle(p, rec);
+                e.Graphics.DrawString(title, this.view.ColumnHeadersDefaultCellStyle.Font, fore, rec, format);
+            }
+        }
+
+        private void Cancel_SortMode()
+        {
+            foreach (DataGridViewColumn col in this.view.Columns) col.SortMode = DataGridViewColumnSortMode.NotSortable;
+        }
+    }
+}
+
+/* 
+ */
